91ff59e77508a7ec434feb74f0ec24204bf41dd6
Byte More <charles.aggasid01@gmail.com>
2024-04-20 16:06:31 -0500

    Implementing the HTTP interaction using Java's HttpClient API

--- src/main/java/com/pluralsight/courseinfo/cli/service/CourseRetrievalService.java
+++ src/main/java/com/pluralsight/courseinfo/cli/service/CourseRetrievalService.java
@@ -7,10 +7,30 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class CourseRetrievalService {
+    //Define a constant for the base URL of the Pluralsight API. Include a placeholder %s for the authorId.
+    private static final String PS_URI = "https://app.pluralsight.com/profile/data/author/sander-mak/all-content";
+
+    //Declare a private static final field for the HttpClient instance.
+    //Initialize it using the default factory method.
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
 
     //Implement method, pass in authorId
     public String getCourserFor(String authorId) {
-        return ""; //return raw JSON string, return "" for now.
-    };
+
+        //Construct the HTTP Request
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create(PS_URI.formatted(authorId)))
+                .GET()
+                .build();
+
+        //Handle Exceptions
+        try {
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body(); //return raw JSON string, return "" for now.
+        } catch (IOException | InterruptedException e) { //Java can catch multi exception
+            throw new RuntimeException("Could not call Pluralsight API", e);
+        }
+    }
 }
 
