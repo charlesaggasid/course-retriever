7160579ba97c8c5ea521f010f2c0bc26f70edfb5
Byte More <charles.aggasid01@gmail.com>
2024-04-20 14:45:39 -0500

    Implement method, pass in authorId
    Return raw JSON string,return "" for now.

---
+++ src/main/java/com/pluralsight/courseinfo/cli/service/CourseRetrievalService.java
@@ -0,0 +1,16 @@
+package com.pluralsight.courseinfo.cli.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class CourseRetrievalService {
+
+    //Implement method, pass in authorId
+    public String getCourserFor(String authorId) {
+        return ""; //return raw JSON string, return "" for now.
+    };
+}
+
