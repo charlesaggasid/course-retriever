d62442533aaba17a0b9cfb636601f9ecf7ebdbf8
Byte More <charles.aggasid01@gmail.com>
2024-04-20 08:30:13 -0500

    adding slf4j dependency

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,23 +1,30 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        System.out.println("CourseRetriever started!");
+        LOG.info("Starting CourseRetriever...");
+
         if (args.length == 0) {
-            System.out.println("Please provide an author name as first argument.");
+            LOG.warn("Please provide an author name as first argument.");
             return;
         }
 
         try {
         retrieveCourses(args[0]); //original one line code, add try catch
         } catch (Exception e) {
-            System.out.println("Unable to retrieve courses from database.");
-            e.printStackTrace();
+            LOG.error("Unable to retrieve courses from database.", e);
+            //e.printStackTrace(); //we can delete this since logger have the throwable e
         }
 
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for " + authorId);
+        LOG.info("Retrieving courses for '{}'", authorId);
     }
 }
